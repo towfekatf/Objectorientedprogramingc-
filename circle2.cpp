72751df2e715b543682bc3c360dd74a26ea1834f
towfekatf <bentatowfeka@gmail.com>
2024-10-06 22:04:30 +0600

    Add files via upload

---
+++ circle2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    float $,r;
+  cout<<"Name: Towfeka Benta towhid\nID:11200120427\nTask:Area of a Circle calculation by using c++\n";
+  cout<<"Enter the radius:";
+
+  cin>>r;
+  $=3.1416*r*r;
+  cout<<"The area of circle is:"<<$;
+
+
+    getch();
+
+}
