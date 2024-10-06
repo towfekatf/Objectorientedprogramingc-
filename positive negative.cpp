0f7769d572919545fd6b233e96552040e24cdd4f
towfekatf <bentatowfeka@gmail.com>
2024-10-06 23:00:26 +0600

    Add files via upload

---
+++ positive negative.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Name:Towfeka Benta towhid\nID:11200120427\nTask:positive or negative\n";
+
+
+    int num = -96;
+
+
+    if (num > 0)
+         cout << "The number is positive";
+    else if (num < 0)
+        cout << "The number is negative";
+    else
+        cout << "Zero";
+
+    return 0;
+}
